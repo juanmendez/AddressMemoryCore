933e73032e7149a41965a0d4555fdcd251bdde53
Juan Mendez <contact@juanmendez.info>
2017-08-14 13:12:06 -0500

    TestingPhotoVP

---
+++ mapmemorycore/src/main/java/info/juanmendez/mapmemorycore/vp/vpPhoto/PhotoPresenter.java
@@ -0,0 +1,10 @@
+package info.juanmendez.mapmemorycore.vp.vpPhoto;
+
+/**
+ * Created by Juan Mendez on 8/14/2017.
+ * www.juanmendez.info
+ * contact@juanmendez.info
+ */
+
+public class PhotoPresenter {
+}

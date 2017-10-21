c69856873485cd1227b5e81e1caf05b3387892dd
Juan Mendez <contact@juanmendez.info>
2017-10-21 16:59:05 -0500

    tested after including RouteMessage

---
+++ addressmemorycore/src/main/java/info/juanmendez/addressmemorycore/models/RouteMessage.java
@@ -0,0 +1,10 @@
+package info.juanmendez.addressmemorycore.models;
+
+/**
+ * Created by Juan Mendez on 10/21/2017.
+ * www.juanmendez.info
+ * contact@juanmendez.info
+ */
+
+public class RouteMessage {
+}

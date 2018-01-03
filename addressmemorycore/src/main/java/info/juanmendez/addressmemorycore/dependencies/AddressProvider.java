89e23abb82f63184390340b985eed422e2c3a7bb
Juan Mendez <contact@juanmendez.info>
2018-01-03 00:01:07 -0600

    AddressProvider connect/disconnect return boolean values

--- addressmemorycore/src/main/java/info/juanmendez/addressmemorycore/dependencies/AddressProvider.java
+++ addressmemorycore/src/main/java/info/juanmendez/addressmemorycore/dependencies/AddressProvider.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import info.juanmendez.addressmemorycore.models.ShortAddress;
 import info.juanmendez.addressmemorycore.models.SubmitError;
-import io.reactivex.Observable;
+import io.reactivex.Flowable;
 
 /**
  * Created by Juan Mendez on 7/7/2017.
@@ -15,8 +15,8 @@ import io.reactivex.Observable;
 public interface AddressProvider {
 
     //these methods are needed for component lifecycle
-    void connect();
-    void disconnect();
+    boolean connect();
+    boolean disconnect();
 
     //getter-setter selectedAddress
     ShortAddress getSelectedAddress();
@@ -24,16 +24,18 @@ public interface AddressProvider {
 
     //get addresses
     List<ShortAddress> getAddresses();
-    Observable<List<ShortAddress>> getAddressesAsync();
+    Flowable<List<ShortAddress>> getAddressesAsync();
 
     //get address
     ShortAddress getAddress(long addressId );
-    Observable<ShortAddress> getAddressAsync(long addressId);
+    Flowable<ShortAddress> getAddressAsync(long addressId);
     List<ShortAddress> getClonedAddresses();
 
     //update address
     ShortAddress updateAddress(ShortAddress address);
     void updateAddressAsync(ShortAddress address, Response<ShortAddress> response );
+
+    void deleteAddress( long addressId );
     void deleteAddressAsync(long addressId, Response<Boolean> response );
 
     //utils
